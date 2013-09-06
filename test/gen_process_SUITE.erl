dc4beac121ab42b14d8d5efd0f762b8a445f547c
Ali Sabil <ali.sabil@soundrop.com>
2013-09-06 12:02:33 +0200

    Add copyright information

--- test/gen_process_SUITE.erl
+++ test/gen_process_SUITE.erl
@@ -1,3 +1,23 @@
+%% This file is based on code from gen_server_SUITE.erl from the R16B1
+%% Erlang/OTP with the following copyright info:
+%%
+%% %CopyrightBegin%
+%%
+%% Copyright Ericsson AB 1996-2013. All Rights Reserved.
+%%
+%% The contents of this file are subject to the Erlang Public License,
+%% Version 1.1, (the "License"); you may not use this file except in
+%% compliance with the License. You should have received a copy of the
+%% Erlang Public License along with this software. If not, it can be
+%% retrieved online at http://www.erlang.org/.
+%%
+%% Software distributed under the License is distributed on an "AS IS"
+%% basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+%% the License for the specific language governing rights and limitations
+%% under the License.
+%%
+%% %CopyrightEnd%
+
 -module(gen_process_SUITE).
 
 -include_lib("common_test/include/ct.hrl").
